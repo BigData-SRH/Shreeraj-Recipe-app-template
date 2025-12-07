e22b6561fe4eaf22567d4b29a4569d5b84ab6501
Shreeraj98 <shreerajpatil98@gmail.com>
2025-12-07 17:37:58 +0100

    Update 02_About us.py

--- pages/02_About us.py
+++ pages/02_About us.py
@@ -81,13 +81,13 @@ def page_about_us():
             
             *Purpose:* Lets the user filter for quick & simple vs. detailed & involved dishes.
 
-            Defination for the complexity levels:
+            **Defination for the complexity levels:**
 
-            A recipe with 1 to 4 steps would be labeled Simple.
+            A recipe with 1 to 4 steps would be labeled **Simple**.
 
-            A recipe with 5 to 8 steps would be labeled Medium.
+            A recipe with 5 to 8 steps would be labeled **Medium**.
 
-            A recipe with 9 or more steps would be labeled Complex.
+            A recipe with 9 or more steps would be labeled **Complex**.
         
         **Future Engineering & Data Preparation** 🛠️
         
